319cc5384409001029f5a2b7df3e11b0fff92317
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-04 12:17:26 -0400

    atv: Label and allow mediashell_app what it needs
    * ATV GMS does this, so we're gonna have to as well.
    
    Change-Id: I0d4fecfad032b0a14a215fa4ddf2e994a9df0c70

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -9,3 +9,6 @@ ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     $(ATV_EVERVOLV_SEPOLICY)/vendor
 endif
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/atv/private
