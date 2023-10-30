LineageOS/android_device_lineage_sepolicy

atv: Allow setupwraith configs to be set by vendor
Change-Id: I0e2831019d5bbb9d432bbad48cd91c98f3c3cb6f

common/sepolicy.mk
@@ -33,3 +33,8 @@ endif
 
 # Selectively include legacy rules defined by the products
 -include device/lineage/sepolicy/legacy-common/sepolicy.mk
+
+# Include atv rules on atv product
+ifeq ($(PRODUCT_IS_ATV), true)
+include device/lineage/sepolicy/atv/sepolicy.mk
+endif

elginsk8r/android_device_evervolv_sepolicy

(cherry picked from commit 72049da6bc559ad5e57a2824b975fba5102258a0)

common/sepolicy.mk
@@ -26,3 +26,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     $(COMMON_EVERVOLV_SEPOLICY)/dynamic \
     $(COMMON_EVERVOLV_SEPOLICY)/vendor
 endif
+
+# Include atv rules on atv product
+ifeq ($(PRODUCT_IS_ATV), true)
+include device/evervolv/sepolicy/atv/sepolicy.mk
+endif